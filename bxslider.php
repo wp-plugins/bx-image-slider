875715e60669dbf77c691300acd9c1b57dc44496
AnjitVishwakarma <AnjitVishwakarma@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-08 11:16:59 +0000

    Performed changes in ui and functions
    git-svn-id: https://plugins.svn.wordpress.org/bx-image-slider/trunk@1261659 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bxslider.php
+++ bxslider.php
@@ -3,7 +3,7 @@
 * Plugin Name:Bx slider
 * Plugin URI: anjitvishwakarma28.wordpress.com
 * Description: this plugin use to make carrosal.
-* Version: 1.0
+* Version: 1.1
 * Author: Anjit Vishwakarma
 * Author URI:anjitvishwakarma28.wordpress.com
 * License: GPL2
@@ -12,9 +12,8 @@
 add_action( 'admin_menu', 'p_init' );
 
 function p_init(){
-add_menu_page('Bx_slider', "Bx Slider", "manage_options", "bxsliders", "bxsliders",plugins_url('ico.png',__FILE__),"3");
-//add_submenu_page( 'artical', 'Add text Artical', 'text', 'manage_options','text','text_type');
-//add_submenu_page( 'artical', 'Add Image Artical', 'Image', 'manage_options','Image','image_type');
+add_menu_page('Bx_slider', "Bx Slider", "manage_options", "bxsliders", "bxsliders",plugins_url('ico.png',__FILE__),"4");
+
 add_action( 'admin_init', 'update_bxsliders' );
 }
 
@@ -61,13 +60,11 @@ endif;
 } 
 add_shortcode( 'bx_Slider', 'bxslider' );
 
-function my_scripts_method() {
+function bx_scripts_method() {
 wp_enqueue_style( 'bxslider',plugins_url('jquery.bxslider.css',__FILE__));
 wp_enqueue_script( 'bxsliderjs',plugins_url('/jquery.bxslider.js',__FILE__));
-wp_enqueue_script( 'bxjs',plugins_url('/bx.js',__FILE__));
 }
-
-add_action( 'wp_footer', 'my_scripts_method' );
+add_action( 'wp_footer', 'bx_scripts_method' );
 if( !function_exists("update_bxsliders") )
 {
 function update_bxsliders() {
@@ -85,38 +82,73 @@ if(isset($_REQUEST["submit"])){
     update_option('bautoStart',sanitize_text_field($_REQUEST['bautoStart']));    
     echo '<div id="message" class="updated fade"><p>Options Updates</p></div>';
 }
-
 function bxsliders(){
 ?>
 <h1>Bx Slider</h1>
 <p>This slider give you advantage to show slider with use of shortcode:</p><p>
 [bx_Slider title='' image='urls of image' description='']</p>
-<p> add titles, images, descriptions in Comma seprated sting like:
+<p> Add titles, images, descriptions in Comma seprated sting like:
 title1,title2......,titlen</p>
 <h2>Settings</h2>
 <form method="post" action="options.php" >
  <?php settings_fields( 'bxsliders-settings' ); ?>
  <?php do_settings_sections( 'bxsliders-settings' ); ?>
   <div class="form-group">
-    <label for="exampleInputEmail1">pager</label>
-    <input type="checkbox" class="form-control"  name="bpager" value="true" <?php if(get_option( 'bpager' )):?> checked <?php endif;?> />
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">controls</label>
-    <input type="checkbox" class="form-control" name="bcontrols" value="true" <?php if(get_option( 'bcontrols' )):?> checked <?php endif;?> >
-  </div>
-<div class="form-group">
-    <label for="exampleInputPassword1">infiniteLoop</label>
-    <input type="checkbox" class="form-control"   name="binfiniteLoop" value="true" <?php if(get_option( 'binfiniteLoop' )):?> checked <?php endif;?> >
-  </div>
-<div class="form-group">
-    <label for="exampleInputPassword1">autoStart</label>
-    <input type="checkbox" class="form-control"   name="bautoStart" value="true" <?php if(get_option( 'bautoStart' )):?> checked <?php endif;?> >
-  </div>
-
+  <table class="form-table">
+<tbody><tr>
+<th scope="row">Default Slider settings</th>
+<td><fieldset><legend class="screen-reader-text"><span>Default Slider settings</span></legend>
+<label>
+<input type="checkbox" class="form-control" name="bcontrols" value="true" <?php if(get_option( 'bcontrols' )):?> checked <?php endif;?> >
+controls</label>
+<br>
+<label>
+ <input type="checkbox" class="form-control"   name="binfiniteLoop" value="true" <?php if(get_option( 'binfiniteLoop' )):?> checked <?php endif;?> >
+infiniteLoop</label>
+<br>
+<label>
+ <input type="checkbox" class="form-control"   name="bautoStart" value="true" <?php if(get_option( 'bautoStart' )):?> checked <?php endif;?> >
+autoStart</label>
+<br>
+<label>
+<input type="checkbox" class="form-control"  name="bpager" value="true" <?php if(get_option( 'bpager' )):?> checked <?php endif;?> />
+pager</label>
+<br>
+<p class="description">(These settings may be overrid the default settings.)</p>
+</fieldset></td>
+</tr>
+</tr>
+</tbody>
+</table>
  <?php submit_button(); ?>
 </form>
+</div>
 <?php
 }
-
-?>
+add_action( 'wp_footer', 'bx_footer_script', 100 );
+function bx_footer_script(){
+  ?>
+  <script>
+  jQuery(document).ready(function(){
+  jQuery('.bxslider').bxSlider({
+<?php if(get_option( 'bpager' )):?>
+ pager: true,
+<?php else: ?>
+ pager: false,
+<?php endif;?> 
+<?php if(get_option( 'bcontrols' )):?>    
+ controls: true,
+<?php else: ?>
+ controls: false, 
+<?php endif;?>      
+<?php if(get_option( 'binfiniteLoop' )):?>   
+infiniteLoop:true,
+<?php endif;?>      
+<?php if(get_option( 'bautoStart' )):?>      
+auto:true,
+<?php endif;?>      
+    });
+});
+  </script>
+  <?php
+}
